647be6110aabe328f217dd7fd4ec38f20640a537
fizban007 <fizban007@gmail.com>
2016-10-03 10:45:12 -0400

    unified helm files up one level key binding

--- spacemacs/.emacs.d/private/alex/packages.el
+++ spacemacs/.emacs.d/private/alex/packages.el
@@ -89,7 +89,8 @@ Each entry is either:
 
     (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
     (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-    (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+    (define-key helm-map (kbd "C-z") 'helm-select-action) ; list actions using C-z
+    (define-key helm-map (kbd "C-h") 'helm-find-files-up-one-level)
     ;; Load helm-mini instead of ido-switch-buffer
     (global-set-key (kbd "C-x b") 'helm-mini)
     (global-set-key (kbd "C-x C-f") 'helm-find-files)
