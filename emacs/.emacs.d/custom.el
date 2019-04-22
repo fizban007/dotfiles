d03c47b2c1db7cb7a18d160515b86636a210f763
fizban007 <fizban007@gmail.com>
2019-04-22 15:30:08 -0400

    Changed emacs configs

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -35,9 +35,10 @@
      (70
       ("\\Phi")))))
  '(company-lighter-base "C")
+ '(company-lsp-enable-snippet t)
  '(custom-safe-themes
    (quote
-    ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
  '(doxymacs-doxygen-style "C++")
  '(eclim-executable "/opt/cuda/libnsight/plugins/org.eclim_1.7.19/bin/eclim")
  '(eclimd-default-workspace "~/Programs")
@@ -45,12 +46,18 @@
  '(linum-disabled-modes-list
    (quote
     (eshell-mode wl-summary-mode compilation-mode text-mode dired-mode)))
+ '(lsp-enable-completion-at-point nil)
+ '(lsp-prefer-flymake nil)
  '(magit-diff-use-overlays nil)
  '(magit-use-overlays nil)
  '(org-export-backends (quote (ascii html icalendar latex md)))
+ '(org-format-latex-options
+   (quote
+    (:foreground default :background default :scale 1.5 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
+		 ("begin" "$1" "$" "$$" "\\(" "\\["))))
  '(package-selected-packages
    (quote
-    (which-key swiper git-commit ggtags evil general find-file-in-project cmake-ide glsl-mode auctex-latexmk magit-popup ivy counsel avy pkgbuild-mode lua-mode company-web company-web-html web-mode visual-fill-column-mode visual-fill-column flx doom-themes modern-cpp-font-lock yasnippet-snippets neotree ivy-rich all-the-icons-ivy spaceline-all-the-icons delight all-the-icons rainbow-delimiters material-theme flycheck-ycmd flycheck elpy cmake-mode smex markdown-mode wgrep-ag ag ace-window pdf-tools htmlize ox-pandoc company-ycmd ycmd cuda-mode cdlatex auctex smartparens smartparens-mode counsel-projectile projectile ghub magit company-c-headers company yasnippet spacemacs-theme atom-one-dark atom-one-dark-theme moe-theme session use-package paradox evil-surround evil-nerd-commenter)))
+    (flymd impatient-mode posframe transient company-lsp emojify webkit-color-picker flymake lsp-ui ccls lsp-mode writeroom-mode eglot tablist dash powerline f with-editor wgrep spaceline async which-key swiper request-deferred request pyvenv ht highlight-indentation goto-chg ggtags evil glsl-mode git-commit general find-file-in-project cmake-ide auctex-latexmk magit-popup ivy counsel avy pkgbuild-mode lua-mode company-web company-web-html web-mode visual-fill-column-mode visual-fill-column flx doom-themes modern-cpp-font-lock yasnippet-snippets neotree ivy-rich all-the-icons-ivy spaceline-all-the-icons delight all-the-icons rainbow-delimiters material-theme flycheck-ycmd flycheck elpy cmake-mode smex markdown-mode wgrep-ag ag ace-window pdf-tools htmlize ox-pandoc company-ycmd ycmd cuda-mode cdlatex auctex smartparens smartparens-mode counsel-projectile projectile ghub magit company-c-headers company yasnippet spacemacs-theme atom-one-dark atom-one-dark-theme moe-theme session use-package paradox evil-surround evil-nerd-commenter)))
  '(projectile-mode-line (quote (:eval (format " P[%s]" (projectile-project-name)))))
  '(projectile-other-file-alist
    (quote
@@ -78,6 +85,10 @@
    (quote
     ((cmake-ide-build-dir . "/home/alex/Projects/Aperture/ninja")
      (cmake-ide-dir . "/home/alex/Projects/Aperture/ninja")
+     (cmake-ide-build-dir . "/home/alex/Projects/Aperture2/ninja")
+     (cmake-ide-dir . "/home/alex/Projects/Aperture2/ninja")
+     (cmake-ide-build-dir . "/home/alex/Projects/1Dpic/ninja")
+     (cmake-ide-dir . "/home/alex/Projects/1Dpic/ninja")
      (zotero-collection . "20")
      ((cmake-ide-build-dir . "/home/alex/Projects/Aperture/ninja")
       (cmake-ide-dir . "/home/alex/Projects/Aperture/ninja")
@@ -86,6 +97,7 @@
       company-clang-arguments "-std=c++11" "-I/home/alex/Programs/Pulsar/include/" "-I/home/alex/Programs/Pulsar/CudaLE/include/"))))
  '(send-mail-function (quote smtpmail-send-it))
  '(session-use-package t nil (session))
+ '(sp-ignore-modes-list (quote (minibuffer-inactive-mode org-mode)))
  '(writeroom-global-effects
    (quote
     (writeroom-toggle-fullscreen writeroom-toggle-alpha writeroom-toggle-menu-bar-lines writeroom-toggle-tool-bar-lines writeroom-toggle-vertical-scroll-bars writeroom-toggle-internal-border-width)))
