629c4300dc5828561556583d745a7b6633c79088
fizban007 <fizban007@gmail.com>
2022-08-15 22:45:40 -0500

    updated zsh config

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -9,14 +9,20 @@ export BROWSER="firefox"
 export XTERM="urxvt"
 export EDITOR="emacsclient -c"
 export GREP_COLOR="1;33"
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=off'
+#export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on'
 #export _JAVA_OPTIONS='-Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 export JAVA_FONTS=/usr/share/fonts/TTF
-export WINEARCH=32
-export WINEPREFIX=~/win32
+#export WINEARCH=win32
+export WINEPREFIX=~/win64
 export DMENU_OPTIONS='-fn xft:monospace-12 -r -x 0 -y 18 -nb #303030 -nf #909090 -sf #303030'
 export TODOTXT_DEFAULT_ACTION=ls
-export PATH="${HOME}/.emacs.d/bin:/opt/visit/bin:$PATH"
+export PATH="${HOME}/.cargo/bin:${HOME}/.emacs.d/bin:${HOME}/.local/bin:/opt/visit/bin:/opt/intel/bin:/usr/lib/ccache/bin:${HOME}/bin:${HOME}/.cabal/bin:$PATH"
+export GTAGSLIBPATH=$HOME/.gtags/
+export AURDEST=$HOME/aur/
+export CUPS_SERVER=gutenberg.astro.princeton.edu:631
+export ANDROID_HOME=/opt/android-sdk
+export NODE_PATH="${HOME}/.npm-packages/lib/node_modules:${NODE_PATH}"
+export LSP_USE_PLISTS=true
 # }}}
 
 # {{{ Dircolors - with rxvt-256color support
